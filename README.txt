9809ea3590c0c5381374c803d79e60561025a7bc
Godefroid Chapelle <gotcha@bubblenet.be>
2014-02-10 16:13:01 +0100

    first import

---
+++ README.txt
@@ -0,0 +1,9 @@
+.. contents::
+
+Introduction
+============
+
+``buildout.eggsdirectories`` includes all eggs found in prepopulated
+directories.
+
+This enables distribution of separate eggs sets.
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(
+    name='buildout.eggsdirectories',
+    version=version,
+    description=(
+        "zc.buildout extension to include supplementary preloaded "
+        "egg-directories"
+    ),
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://github.com/collective/buildout.eggsdirectories',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['buildout'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+[zc.buildout.extension]
+buildout.eggsdirectories = buildout.eggsdirectories:extend
+    """,
+)
